cd8bc08ed7bc87d09c45b0f5aef82108b29f69c5
Cesar Perez <Cesar.Perez5272@yahoo.com>
2020-04-06 20:34:17 -0400

    'relationship issue'

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,15 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
+  validateChecksums: false,
   "connectionString": (process.env.NODE_ENV === 'test')
   ? process.env.TEST_DB_URL
   : process.env.DB_URL,
 }
+
+// const postgrator = new Postgrator({
+//   validateChecksums: true, // Set to false to skip validation
+//   newline: 'CRLF' // Force using 'CRLF' (windows) or 'LF' (unix/mac)
+// })
+
+// module.exports = postgrator
